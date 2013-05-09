9e27e4e9a082211fa0bda5832250eefb5a085afa
Michael Still <mikal@stillhq.com>
2013-05-09 12:57:47 +1000

    Listen to gerrit events and log them.
    The idea here is to have a few of these running, and then have a
    central job which merges these logs. That way if a cluster goes
    offline or I lose a single worker I wont lose elements from the
    stream.

---
+++ eventlistener.py
@@ -0,0 +1,67 @@
+#!/usr/bin/python
+
+# Listen to events from gerrit and log them to files. Run more than
+# one of these, as a failure will cause a gap in the event stream.
+
+import datetime
+import os
+import paramiko
+import sys
+import time
+
+
+hostname = 'review.openstack.org'
+hostport = 29418
+username = 'mikalstill'
+keyfile = '/home/mikal/.ssh/id_gerrit'
+
+
+def stream_events():
+    # Connect
+    transport = paramiko.Transport((hostname, hostport))
+    transport.start_client()
+
+    # Authenticate with the key
+    key = paramiko.RSAKey.from_private_key_file(keyfile)
+    transport.auth_publickey(username, key)
+
+    channel = transport.open_session()
+    channel.exec_command('gerrit stream-events')
+
+    print '%s Connected' % datetime.datetime.now()
+    data = ''
+
+    try:
+        while True:
+            if not channel.recv_ready():
+                time.sleep(1)
+
+            else:
+                d = channel.recv(1024)
+                if not d:
+                    print '%s Connection closed' % datetime.datetime.now()
+                    return
+
+                print '%s Read %d bytes' %(datetime.datetime.now(), len(d))
+                data += d
+
+                if data.find('\n') != -1:
+                    lines = data.split('\n')
+                    for line in lines[:-1]:
+                        now = datetime.datetime.now()
+                        path = os.path.join('output', str(now.year),
+                                            str(now.month))
+                        if not os.path.exists(path):
+                            os.makedirs(path)
+                        with open(os.path.join(path, str(now.day)), 'a') as f:
+                            f.write('%s\n' % line)
+
+                    data = lines[-1]
+
+    finally:
+        transport.close()
+
+
+if __name__ == '__main__':
+    while True:
+        stream_events()
