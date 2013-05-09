50bcfde5a5b945798895e7ca1118ec0a36c9c06c
Michael Still <mikal@stillhq.com>
2013-05-09 14:27:26 +1000

    Collect events in a database.

---
+++ eventparser.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python
+
+# Parse the logs written by the listener into events we want to run things on.
+# These events are stored in a mysql database.
+
+import datetime
+import json
+import MySQLdb
+import urllib
+
+import utils
+
+
+def fetch_log_day(cursor, dt):
+    new = 0
+
+    for host in ['50.56.178.145', '198.61.229.73']:
+        remote = urllib.urlopen('http://%s/output/%s/%s/%s'
+                                %(host, dt.year, dt.month, dt.day))
+        for line in remote.readlines():
+            packet = json.loads(line)
+            if packet.get('type') == 'patchset-created':
+                cursor.execute('insert ignore into patchsets '
+                               '(id, project, number, refurl, git_fetched) '
+                               'values ("%s", "%s", %d, "%s", 0);'
+                               %(packet['change']['id'],
+                                 packet['change']['project'],
+                                 packet['patchSet']['number'],
+                                 packet['patchSet']['ref']))
+                new += cursor.rowcount
+                cursor.execute('commit;')
+
+    return new
+
+
+if __name__ == '__main__':
+    cursor = utils.GetCursor()
+    now = datetime.datetime.now()
+    new = 0
+    
+    for i in range(7):
+        try:
+            new += fetch_log_day(cursor, now)
+        except:
+            pass
+
+        now -= datetime.timedelta(days=1)
+
+    print 'Added %d new patchsets' % new
---
+++ utils.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+
+def GetCursor():
+    # Read config from a file
+    with open('/srv/config/gerritevents') as f:
+        flags = json.loads(f.read())
+
+    db = MySQLdb.connect(user = flags['dbuser'],
+                         db = flags['dbname'],
+                         passwd = flags['dbpassword'])
+    cursor = db.cursor(MySQLdb.cursors.DictCursor)
+    return cursor
