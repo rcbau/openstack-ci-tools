a38289d2b16e38c6a6559d868026234dedfa33ee
Michael Still <mikal@stillhq.com>
2013-06-05 08:37:19 +0000

    Simple script to dump logs to a web server

---
+++ dumper.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python
+
+# Dump logs of jobs to a web server
+
+import os
+
+import utils
+
+if __name__ == '__main__':
+    cursor = utils.get_cursor()
+    subcursor = utils.get_cursor()
+    cursor.execute('select * from work_queue where done is not null;')
+    for row in cursor:
+        path = os.path.join('/var/www/ci', row['id'], str(row['number']), row['workname'])
+        print path
+        workerpath = os.path.join(path, 'worker')
+        worker = None
+        if os.path.exists(workerpath):
+            with open(workerpath, 'r') as f:
+                worker = f.read().rstrip()
+
+        if worker != row['worker']:
+            if not os.path.exists(path):
+                os.makedirs(path)
+            with open(workerpath, 'w') as f:
+                f.write(row['worker'])
+            with open(os.path.join(path, 'state'), 'w') as f:
+                f.write(row['done'])
+            with open(os.path.join(path, 'log'), 'w') as f:
+                subcursor.execute('select * from work_logs where id="%s" and number=%s and workname="%s" and worker="%s" order by timestamp asc;'
+                                  %(row['id'], row['number'], row['workname'], row['worker']))
+                for logrow in subcursor:
+                    f.write('%s %s\n' %(logrow['timestamp'], logrow['log']))
