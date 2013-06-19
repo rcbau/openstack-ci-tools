d85377e9a399403d6a63f7e2aa2e2ae81a58babc
Michael Still <mikal@stillhq.com>
2013-06-19 06:47:12 +0000

    Simple setup script

---
+++ setup.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+apt-get install git python-pip git-review libxml2-dev libxml2-utils libxslt-dev libmysqlclient-dev pep8 postgresql-server-dev-9.1 python2.7-dev python-coverage python-netaddr python-mysqldb mysql-server
