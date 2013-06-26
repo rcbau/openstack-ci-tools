d99f3d48801e4619d86b9badd3824c45e7b6b7ee
Michael Still <mikal@stillhq.com>
2013-06-26 06:08:30 +0000

    Script volume creation

---
+++ setup_volume.sh
@@ -0,0 +1,25 @@
+#!/bin/bash -ex
+
+cp etc/volumes /etc/network/if-up.d/
+chmod ugo+rx /etc/network/if-up.d/
+pvcreate /dev/xvdb
+vgcreate srv /dev/xvdb
+lvcreate -L99G -nsrv srv
+lvcreate -L100G -nmysql srv
+mkfs.ext4 /dev/mapper/srv-srv 
+mkfs.ext4 /dev/mapper/srv-mysql
+/etc/network/if-up.d/volumes 
+mkdir /srv/mysql
+/etc/network/if-up.d/volumes 
+df -h
+
+chown -R mikal.mikal /srv
+mkdir /srv/git
+mkdir /srv/git-checkouts
+mkdir /srv/logs
+
+scp -rp gerrit-event-slave.stillhq.com:/srv/mysql/* /srv/mysql/
+scp -rp gerrit-event-slave.stillhq.com:/srv/[cd]* /srv/
+
+chown -R mikal.mikal /srv
+chown -R mysql.mysql /srv/mysql
