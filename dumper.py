1a3da40bd9cf881e9f0b9a32a6b6fc1685f9f411
Michael Still <mikal@stillhq.com>
2013-07-07 18:56:42 +1000

    Handle percona retries, and tweak the dumper to know about percona

--- dumper.py
+++ dumper.py
@@ -9,22 +9,8 @@ import os
 import re
 
 import utils
+import workunit
 
-
-# Remember that the timestamp isn't actually part of the log row!
-UPGRADE_BEGIN_RE = re.compile('\*+ DB upgrade to state of (.*) starts \*+')
-UPGRADE_END_RE = re.compile('\*+ DB upgrade to state of (.*) finished \*+')
-
-GIT_CHECKOUT_RE = re.compile('/srv/git-checkouts/[a-z]+/'
-                             '[a-z]+_refs_changes_[0-9_]+')
-VENV_PATH_RE = re.compile('/home/mikal/\.virtualenvs/refs_changes_[0-9_]+')
-
-MIGRATION_START_RE = re.compile('([0-9]+) -&gt; ([0-9]+)\.\.\.$')
-MIGRATION_END_RE = re.compile('^done$')
-
-FINAL_VERSION_RE = re.compile('Final schema version is ([0-9]+)')
-MIGRATION_CLASH_RE = re.compile('Error: migration number .* appears '
-                                'more than once')
 
 NEW_RESULT_EMAIL = """Results for a test are available.
 
@@ -181,185 +167,13 @@ if __name__ == '__main__':
 
     cursor = utils.get_cursor()
     subcursor = utils.get_cursor()
-    subsubcursor = utils.get_cursor()
 
     # Write out individual work logs
     cursor.execute('select * from work_queue where done is not null;')
     for row in cursor:
-        path = os.path.join('/var/www/ci', row['id'], str(row['number']),
-                            row['workname'])
-        path += utils.format_attempt_path(row['attempt'])
-
-        datapath = os.path.join(path, 'data')
-        workerpath = os.path.join(path, 'worker')
-        worker = None
-        if os.path.exists(workerpath):
-            with open(workerpath, 'r') as f:
-                worker = f.read().rstrip()
-
-        if worker != row['worker']:
-            print path
-            if not os.path.exists(path):
-                os.makedirs(path)
-            with open(workerpath, 'w') as f:
-                f.write(row['worker'])
-            with open(os.path.join(path, 'state'), 'w') as f:
-                f.write(row['done'])
-            with open(os.path.join(path, 'log.html'), 'w') as f:
-                buffered = []
-                upgrades = []
-                upgrade_times = {}
-                in_upgrade = False
-                migration_start = None
-                final_version = None
-
-                subcursor.execute('select * from work_logs where id="%s" and '
-                                  'number=%s and workname="%s" and '
-                                  'worker="%s" and %s order by timestamp asc;'
-                                  %(row['id'], row['number'], row['workname'],
-                                    row['worker'], row['attempt'])))
-                linecount = 0
-                f.write('<html><head><title>%(id)s -- %(number)s</title>\n'
-                        '<link rel="stylesheet" type="text/css" '
-                        'href="/style.css" />\n'
-                        '</head><body>\n'
-                        '<h1>CI run for %(id)s, patchset %(number)s</h1>\n'
-                        '<p>What is this? This page shows the logs from a '
-                        'database upgrade continuous integration run. Each '
-                        'patchset which proposes a database migration is run '
-                        'against a set of test databases. This page shows the '
-                        'results for one of those test databases. If the '
-                        'database is from Folsom, you will see a Grizzly '
-                        'migration in the bullet list below. You should then '
-                        'see an upgrade to the current state of trunk, and '
-                        'then finally the upgrade(s) contained in the '
-                        'patchset. For more information, please contact '
-                        '<a href="mailto:mikal@stillhq.com">'
-                        'mikal@stillhq.com</a>.</p>\n'
-                        % {'id': row['id'],
-                           'number': row['number']})
-
-                data = {}
-                for logrow in subcursor:
-                    m = FINAL_VERSION_RE.match(logrow['log'])
-                    if m:
-                         final_version = int(m.group(1))
-
-                    m = UPGRADE_BEGIN_RE.match(logrow['log'])
-                    if m:
-                         upgrade_name = m.group(1)
-                         upgrades.append(upgrade_name)
-                         upgrade_start = logrow['timestamp']
-                         in_upgrade = True
-
-                         buffered.append('<a name="%s"></a>' % upgrade_name)
-
-                    m = MIGRATION_CLASH_RE.match(logrow['log'])
-                    if m:
-                        data['color'] = 'bgcolor="#FA5858"'
-                        data['result'] = 'Failed: migration number clash'
-                        print '    Failed'
-
-                    line = ('<a name="%(linenum)s"></a>'
-                            '<a href="#%(linenum)s">#</a> '
-                            % {'linenum': linecount})
-                    if in_upgrade:
-                        line += '<b>'
-
-                    cleaned = logrow['log'].rstrip()
-                    cleaned = cleaned.replace('/srv/openstack-ci-tools', '...')
-                    cleaned = GIT_CHECKOUT_RE.sub('...git...', cleaned)
-                    cleaned = VENV_PATH_RE.sub('...venv...', cleaned)
-                    cleaned = cgi.escape(cleaned)
-
-                    m = MIGRATION_END_RE.match(cleaned)
-                    if m and migration_start:
-                        elapsed = logrow['timestamp'] - migration_start
-                        cleaned += ('              <font color="red">[%s]'
-                                    '</font>'
-                                    % timedelta_as_str(elapsed))
-                        migration_start = None
-
-                    m = MIGRATION_START_RE.match(cleaned)
-                    if m:
-                        migration_start = logrow['timestamp']
-                        subsubcursor.execute('select * from '
-                                             'patchset_migrations '
-                                             'where id="%s" and number=%s and '
-                                             'migration=%s;'
-                                             %(row['id'], row['number'],
-                                               m.group(2)))
-                        subsubrow = subsubcursor.fetchone()
-                        if subsubrow:
-                            cleaned += ('     <font color="red">[%s]</font>'
-                                        % subsubrow['name'])
-
-                    line += ('%(timestamp)s %(line)s'
-                             % {'timestamp': logrow['timestamp'],
-                                'line': cleaned})
-                    if in_upgrade:
-                        line += '</b>'
-                    line += '\n'
-                    buffered.append(line)
-
-                    linecount += 1
-
-                    m = UPGRADE_END_RE.match(logrow['log'])
-                    if m:
-                         in_upgrade = False
-                         elapsed = logrow['timestamp'] - upgrade_start
-                         elapsed_str = timedelta_as_str(elapsed)
-                         buffered.append('                                   '
-                                         '     <font color="red"><b>'
-                                         '[%s total]</b></font>\n'
-                                          % elapsed_str)
-                         upgrade_times[upgrade_name] = elapsed
-
-                display_upgrades = []
-                data.update({'order': upgrades,
-                             'details' : {},
-                             'details_seconds': {},
-                             'final_schema_version': final_version})
-                for upgrade in upgrades:
-                    time_str = timedelta_as_str(upgrade_times[upgrade])
-                    display_upgrades.append('<li><a href="#%(name)s">'
-                                            'Upgrade to %(name)s -- '
-                                            '%(elapsed)s</a>'
-                                            % {'name': upgrade,
-                                               'elapsed': time_str})
-                    data['details'][upgrade] = time_str
-                    data['details_seconds'][upgrade] = \
-                        upgrade_times[upgrade].seconds
-                    data['color'] = ''
-
-                    print '    %s (%s)' %(upgrade,
-                                          upgrade_times[upgrade].seconds)
-                    if upgrade == 'patchset':
-                        if upgrade_times[upgrade].seconds > 30:
-                            data['color'] = 'bgcolor="#FA5858"'
-                            data['result'] = 'Failed: patchset too slow'
-                            print '        Failed'
-
-                if final_version:
-                    subsubcursor.execute('select max(migration) from '
-                                         'patchset_migrations where id="%s" '
-                                         'and number=%s;'
-                                         %(row['id'], row['number']))
-                    subsubrow = subsubcursor.fetchone()
-                    data['expected_final_schema_version'] = \
-                        subsubrow['max(migration)']
-                    if final_version != subsubrow['max(migration)']:
-                        data['color'] = 'bgcolor="#FA5858"'
-                        data['result'] = 'Failed: incorrect final version'
-                        print '        Failed'
-
-                f.write('<ul>%s</ul>' % ('\n'.join(display_upgrades)))
-                f.write('<pre><code>\n')
-                f.write(''.join(buffered))
-                f.write('</code></pre></body></html>')
-
-                with open(datapath, 'w') as d:
-                    d.write(json.dumps(data))
+        work = workunit.WorkUnit(row['ident'], row['number'],
+                                 row['constraints'], row['attempt'])
+        work.persist_to_disk(subcursor)
 
     # Write out an index file
     write_index('select * from work_queue order by heartbeat desc limit 100;',
