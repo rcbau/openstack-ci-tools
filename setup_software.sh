0bf10d36231e70c46b15249e1d30d9c12c29b3db
Michael Still <mikal@stillhq.com>
2013-07-07 22:34:31 +1000

    Working on percona support

--- setup_software.sh
+++ setup_software.sh
@@ -11,7 +11,7 @@ apt-key adv --keyserver keys.gnupg.net --recv-keys 1C4CBDCDCD2EFD2A
 cp etc/percona.list /etc/apt/sources.list.d/percona.list
 
 apt-get update
-apt-get dist-upgrade
+apt-get dist-upgrade -y
 apt-get install -y git python-pip git-review libxml2-dev libxml2-utils libxslt-dev libmysqlclient-dev pep8 postgresql-server-dev-9.1 python2.7-dev python-coverage python-netaddr python-mysqldb $1-server python-git virtualenvwrapper python-numpy
 
 cp etc/my.cnf /etc/mysql/
